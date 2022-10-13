2d586a2eddd5af0ae6b32a04164b6787a8d6eefa
tidus <gun7220@gmail.com>
2022-10-13 14:34:40 +0900

    H2 db 추가, JPA설정 및 API 추가

--- src/main/java/com/example/springbootstarter/sample/SampleController.java
+++ src/main/java/com/example/springbootstarter/sample/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.springbootstarter.sample;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    @Autowired
+    SampleService sampleService;
+
     @GetMapping
     ResponseEntity<String> home() {
         return ResponseEntity.ok("Hello World!");
+    }
+
+    @GetMapping("/sample")
+    ResponseEntity<SampleEntity> sample() {
+        return ResponseEntity.ok(this.sampleService.sampleService());
     }
 
 }
